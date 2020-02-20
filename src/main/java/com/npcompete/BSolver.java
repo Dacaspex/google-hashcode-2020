b794406f68b459804f201ff4981e7f83a12f899f
Casper Smits <caspersmits11@outlook.com>
2020-02-20 19:42:54 +0100

    b test

--- src/main/java/com/npcompete/BSolver.java
+++ src/main/java/com/npcompete/BSolver.java
@@ -5,6 +5,28 @@ import java.util.Comparator;
 import java.util.List;
 
 public class BSolver {
+    public void test(List<Library> libraries, List<Book> books) {
+        // all books are 100
+        libraries.forEach(library -> {
+            library.books.forEach(book -> {
+                if (book.score != 100) {
+                    System.out.println(book.id);
+                }
+            });
+
+            // all libraries same books, same shipping time
+            if (library.scan_capacity != 1) {
+                System.out.println(library.id);
+            }
+
+            if (library.books.size() != 1000) {
+                System.out.println(library.id);
+            }
+        });
+
+        System.out.println("all good");
+    }
+
     public void solve(List<Library> libraries, List<Book> books) {
         // Sort libraries on increasing sign up time
         libraries.sort(Comparator.comparingInt(l -> l.signup_time));
