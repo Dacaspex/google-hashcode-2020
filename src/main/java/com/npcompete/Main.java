8e13f0c7f0d232ba2f51f47be8998ee1ccf8f1ee
Casper Smits <caspersmits11@outlook.com>
2020-02-20 09:16:27 +0100

    setup

---
+++ src/main/java/com/npcompete/Main.java
@@ -0,0 +1,19 @@
+package com.npcompete;
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner;
+
+    public Main() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        (new Main()).run();
+    }
+}
